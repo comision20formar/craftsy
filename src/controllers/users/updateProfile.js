12bfdf438c85d42ac4f2bed438c84e142b17ed86
Eric <menaericdaniel@gmail.com>
2023-09-22 20:46:23 -0300

    feat : add updated user

--- src/controllers/users/updateProfile.js
+++ src/controllers/users/updateProfile.js
@@ -1,3 +1,22 @@
+const { readJSON, writeJSON } = require("../../data")
+
 module.exports = (req,res) => {
-    return res.send(req.body)
+    const users = readJSON('users.json');
+    const {name, surname, birthday, gender, about} = req.body
+    const usersUpdated = users.map(user => {
+        if(user.id === req.session.userLogin.id){
+            return {
+                ...user,
+                name : name.trim(),
+                surname: surname.trim(),
+                birthday,
+                gender,
+                about : about.trim()
+            }
+        }
+        return user
+    })
+
+    writeJSON(usersUpdated, 'users.json');
+    return res.redirect('/')
 }
