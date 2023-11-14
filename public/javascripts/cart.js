11f5077cc8012334f9358c857501f6fe1a569486
Eric <menaericdaniel@gmail.com>
2023-11-14 06:58:45 -0300

    feature: add product in cart

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,21 +1,91 @@
-const $ = id => document.getElementById(id)
+const $ = (id) => document.getElementById(id);
+
+const addItemToCart = async (quantity, product) => {
+  try {
+    const response = await fetch("/cart", {
+      method: "POST",
+      body: JSON.stringify({
+        quantity,
+        product,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { ok, cart, message } = await response.json();
+
+    if (!ok) {
+      throw new Error(message);
+    }
+  } catch (error) {
+    Swal.fire({
+      title: "Upss",
+      text: error.message,
+      icon: "error",
+    });
+  }
+};
 
 window.onload = function () {
+  $("btn-cart") &&
+    $("btn-cart").addEventListener("click", async function (e) {
+      try {
+        const response = await fetch("/cart");
+        const { ok, cart, message } = await response.json();
 
-
-    $('btn-cart') && $('btn-cart').addEventListener('click', async function (e) {
-        
-        try {
-
-            const response = await fetch('/cart');
-            const result = await response.json();
-
-            console.log(result);
+        if (ok) {
+          if (cart.products.length) {
+            $("cart-body").innerHTML = `
             
-        } catch (error) {
-            console.log(error);
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Precio</th>
+                    </tr>
+                </thead>
+                <tbody id="cart-table">
+                  
+                </tbody>
+            </table>
+
+            `;
+
+            
+
+            cart.products.forEach(({image, name,price,discount, quantity}) => {
+                $('cart-table').innerHTML += `
+                <tr>
+                    <th scope="row">
+                    <img src="/images/${image}" width=100 alt="" />
+                    </th>
+                    <td>${name}</td>
+                    <td>${quantity}</td>
+                    <td>${price}</td>
+                </tr>
+                `
+            });
+
+          } else {
+            $("cart-body").innerHTML = `
+                    <div class="alert alert-warning" role="alert">
+                        No hay productos en el carrito.
+                    </div>
+                  `;
+          }
+        } else {
+          throw new Error(message);
         }
-    })
-
-
-}
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          title: "Upss",
+          text: message,
+          icon: "error",
+        });
+      }
+    });
+};
--- src/controllers/apiCartController.js
+++ src/controllers/apiCartController.js
@@ -1,29 +1,85 @@
+const db = require('../database/models');
+
 module.exports = {
     showAll : async (req,res) => {
         try {
 
             if(!req.session.cart) {
                 let error = new Error()
-                error.message = 'Sesión no encontrada';
+                error.message = 'Debes loguearte';
                 error.status = 404;
                 throw error
             }
 
             return res.status(200).json({
                 ok : true,
-                cart : req.session.cart
+                cart : req.session.cart,
+                message : "ok"
             })
 
         } catch (error) {
               return res.status(error.status || 500).json({
                 ok : false,
+                cart : null,
                 message : error.message || 'Upss, hubo un error'
             })
         }
      
     },
     addItem : async (req,res) => {
+        try {
 
+            if(!req.session.cart) {
+                let error = new Error()
+                error.message = 'Debes loguearte';
+                error.status = 404;
+                throw error
+            }
+
+            console.log(req.body)
+
+            const {quantity, product: id} = req.body;
+
+            const {name, image, price, discount} = await db.Product.findByPk(id);
+
+            if(req.session.cart.products.map(product => product.id).includes(id)){
+
+                req.session.cart.products = req.session.cart.products.map(product => {
+                    if(product.id === id){
+                        ++product.quantity
+                    }
+                    return product
+                });
+
+            }else{
+
+                req.session.cart.products.push({
+                    id,
+                    name,
+                    image,
+                    price,
+                    discount,
+                    quantity,
+                });
+            }
+
+         
+
+            req.session.total = req.session.cart.products.map(product => product.price * quantity).reduce((a,b) => a + b, 0)
+
+            return res.status(200).json({
+                ok : true,
+                cart : req.session.cart,
+                message : "ok"
+            })
+            
+        } catch (error) {
+            return res.status(error.status || 500).json({
+                ok : false,
+                cart : null,
+                message : error.message || 'Upss, hubo un error'
+            })
+        }
     },
     removeItem : async (req,res) => {
 
